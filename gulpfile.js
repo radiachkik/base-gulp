5d4425b4f7e93f941dafe66d18f449631762053b
Williamium <william@williamwalker.me>
2016-02-07 15:59:59 +0000

    Re-organised package.json and gulpfile requires in grouped alphabetical order. Removed image optimisation tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,27 @@
 // Load Gulp and all the plugins
-var gulp    = require('gulp'),
-concat      = require('gulp-concat'),
-uglify      = require('gulp-uglify'),
-sourcemaps  = require('gulp-sourcemaps'),
+var pkg     = require('./package.json'),
+browserSync = require('browser-sync'),
+dateFormat  = require('dateformat'),
+reload      = browserSync.reload,
+gulp        = require('gulp'),
 autoprefix  = require('gulp-autoprefixer'),
-minifycss   = require('gulp-minify-css'),
+concat      = require('gulp-concat'),
+filter      = require('gulp-filter'),
 header      = require('gulp-header'),
 jshint      = require('gulp-jshint'),
+minifycss   = require('gulp-minify-css'),
 notify      = require('gulp-notify'),
 rename      = require('gulp-rename'),
-filter      = require('gulp-filter'),
-changed     = require('gulp-changed'),
 sass        = require('gulp-sass'),
-imagemin    = require('gulp-imagemin'),
-pngquant    = require('imagemin-pngquant'),
-mozjpeg     = require('imagemin-mozjpeg'),
-browserSync = require('browser-sync'),
-reload      = browserSync.reload;
+sourcemaps  = require('gulp-sourcemaps'),
+uglify      = require('gulp-uglify');
 
 // Set the banner
-var pkg    = require('./package.json'),
-dateFormat = require('dateformat'),
-now        = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss Z"),
-banner     = '/*!\n'+
-			 ' * <%= pkg.name %>\n'+
-			 ' * @author <%= pkg.author %>\n'+
-			 ' * @version <%= pkg.version %>\n'+
-			 ' * Build date: '+ now +'\n'+
-			 ' */\n';
+var now = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss Z"),
+banner  = '/*!\n'+
+		  ' * <%= pkg.name %>\n'+
+		  ' * Build date: '+ now +'\n'+
+		  ' */\n';
 
 // JS hint task: Runs JSHint using the options in the .jshintrc file
 gulp.task('jshint', function() {
@@ -133,25 +127,6 @@ gulp.task('plugin-styles', function() {
 		.pipe(notify(function (file) {
 			return 'Styles: ' + file.relative + ' generated.';
 		}));
-});
-
-// Image optimisation task: optimises jpegs and pngs
-gulp.task('optimise-images', function () {
-	gulp.src('img/src/**/*')
-		.pipe(changed('img/build/'))
-		.pipe(imagemin({
-			progressive: true,
-			optimizationLevel: 5,
-			use: [pngquant({ quality: '60-80' }), mozjpeg()]
-		}))
-		.pipe(gulp.dest('img/build/'));
-});
-
-gulp.task('generate-webp-images', function () {
-	gulp.src('img/src/**/*.jpg') // We'll only use it for jpegs since we get better results using pngquant above for pngs
-		.pipe(changed('img/build/', { extension: '.webp' }))
-		.pipe(webp({ quality: 83 })) // I think default quality is 75 which is a bit too low (especially for use in some PNGs)
-		.pipe(gulp.dest('img/build/'));
 });
 
 // Configure and start BrowserSync
